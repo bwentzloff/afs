d922603d93aee77e5a3dfd5010404f58f78dd609
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-01-12 12:10:31 -0600

    homepage message

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -10,11 +10,8 @@
         <div class="card card-default">
             <div class="card-body">
                 <p>
-                    XFL season-long fantasy leagues are coming today!! We just have to wait for some DNS records to go through and a security certificate to get set up and we'll be good to go. Check back this afternoon and everything should be ready. I'm writing this on Sunday, Jan 12, by the way.
-                    </p>
-                <!--p>
                     XFL season-long fantasy leagues are forming now. Register or Login above to start one. If you had an account last year, you will need to create a new account. Account security was updated this summer. I will have a tool out soon to connect your new account to your old one and get all the data back.
-                </p-->
+                </p>
             </div>
         </div>
     </div>
