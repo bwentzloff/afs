647caefd71e4e91a947c31adbebccf94025c02be
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-01-29 14:16:07 -0600

    Make it clear you're updating the draft

--- resources/js/pages/ViewLeague.vue
+++ resources/js/pages/ViewLeague.vue
@@ -403,7 +403,7 @@
                                     <label for="drafttime">Update Draft Date/Time <small><em>Times shown are in your local timezone. Other members of your league will have the time converted to their local timezone.</em></small></label>
                                     <datetime v-model="updateDraftDatetime" type="datetime" zone="local" value-zone="UTC" :use12-hour=true title="Draft Time" name="draft_datetime"></datetime>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Update</button>
+                                <button type="submit" class="btn btn-primary">Update Draft Date/Time</button>
                             </form>
                             <h2>Draft Order</h2>
                             <b-table
