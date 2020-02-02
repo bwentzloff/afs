3c78db28889c0431b6d00a626b3c2c89d3bc2e67
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-02-01 21:06:17 -0600

    fix matchups

--- app/Http/Controllers/LeagueController.php
+++ app/Http/Controllers/LeagueController.php
@@ -486,7 +486,7 @@ class LeagueController extends Controller
             $leagueUser->save();
 
             $this->setDraftOrder($league->id);
-            if ($league->draft_status > 1) $matchups = Matchup::where('league_id',$request->input('leagueId'))->delete();
+            $matchups = Matchup::where('league_id',$request->input('leagueId'))->delete();
             $lastUpdate = uniqid();
             Cache::put('leagueUpdate'.$league->id, $lastUpdate,600);
         }
