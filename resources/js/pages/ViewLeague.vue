d8a9911dcccaff481e1695f06783fcf51527d8f5
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-02-08 10:26:10 -0600

    adjust time frame

--- resources/js/pages/ViewLeague.vue
+++ resources/js/pages/ViewLeague.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div>
             <b-card bg-variant="danger" text-variant="black">
-                Expect high server loads and site slowdowns Friday night and Saturday morning. Make sure your Draft Queue is set. Stats for the first game will be delayed 1-2 hours. After that, we'll have real time scoring.
+                Expect high server loads and site slowdowns Friday night and Saturday morning. Make sure your Draft Queue is set. Stats for the first game will be delayed several hours. After that, we'll have real time scoring.
             </b-card>
             <b-card bg-variant="dark" text-variant="white" v-if="preDraft">
                 <b-card-body>
