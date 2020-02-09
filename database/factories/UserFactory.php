82005689eca7a679cbebaaa758fa94ea6c44129d
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-02-09 07:23:14 -0800

    Update User Model Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,12 +15,23 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->defineAs(User::class, 'user', function ($faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        // -1 for super admin, 1 for admin, 0 for user
+        'role' => 0,
     ];
+});
+
+$factory->defineAs(User::class, 'user_admin', function ($faker) use ($factory) {
+    return array_merge($factory->raw(User::class), ['role' => 1]);
+});
+
+$factory->defineAs(User::class, 'user_super_admin', function ($faker) use ($factory) {
+    $user = ;
+    return array_merge($factory->raw(User::class), ['role' => -1]);
 });
