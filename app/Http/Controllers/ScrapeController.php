d27b9389848375e1c0714173c604e16e155b04a5
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-02-10 09:14:57 -0600

    get feedback from score calculator

--- app/Http/Controllers/ScrapeController.php
+++ app/Http/Controllers/ScrapeController.php
@@ -61,7 +61,7 @@ class ScrapeController extends Controller
             ->get();
 
         foreach($leagues as $league) {
-            print($league->id);
+            print($league->id."<br />");
             $sport = Sport::where('id',8)->first();
             $matchups = Matchup::where('league_id',$league->id)
                 ->where('week',$sport->current_week)
