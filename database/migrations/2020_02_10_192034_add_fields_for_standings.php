6a03bfa03e2fc92944d60e9166859ea17712e054
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-02-10 16:12:22 -0600

    fix conflicting migration name

--- database/migrations/2020_02_10_192034_add_fields_for_standings.php
+++ database/migrations/2020_02_10_192034_add_fields_for_standings.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFieldsForStandings extends Migration
+class AddFieldsForStandingsUpdate extends Migration
 {
     /**
      * Run the migrations.
