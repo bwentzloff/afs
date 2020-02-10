607dd6ffc79bd5e5c9d68186686375619d9da144
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-02-10 15:47:59 -0600

    show away flex players

--- resources/js/pages/ViewLeague.vue
+++ resources/js/pages/ViewLeague.vue
@@ -1962,12 +1962,12 @@ import moment from 'moment'
                 }
             }
 
-            for (var lineup = 0; lineup < this.matchup_away_qb_starters.length; lineup++) {
+            for (var lineup = 0; lineup < this.matchup_away_flex_starters.length; lineup++) {
                 this.matchup_away_qb_starters[lineup].statline = []
                 for (var player_score = 0; player_score < this.matchup_player_stats.length; player_score++) {
-                    if (this.matchup_away_qb_starters[lineup].player_id == this.matchup_player_stats[player_score].player_id) {
-                        this.matchup_away_qb_starters[lineup].score = this.calculatePlayerScore(this.matchup_player_stats[player_score])
-                        this.matchup_away_qb_starters[lineup].statline = this.getPlayerStatline(this.matchup_player_stats[player_score])
+                    if (this.matchup_away_flex_starters[lineup].player_id == this.matchup_player_stats[player_score].player_id) {
+                        this.matchup_away_flex_starters[lineup].score = this.calculatePlayerScore(this.matchup_player_stats[player_score])
+                        this.matchup_away_flex_starters[lineup].statline = this.getPlayerStatline(this.matchup_player_stats[player_score])
                     }
                 }
             }
