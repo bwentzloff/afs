49daebf5f86b6a87e85707d195a32e325f3a1abc
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-02-19 00:21:15 -0800

    Add Reset Password test

--- tests/Feature/Http/Controllers/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/AuthControllerTest.php
@@ -6,33 +6,81 @@ use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class AuthControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * Test API Login endpoint.
+     * Test Login endpoint.
+     *
+     * Verifies the given email and password exist
+     * for a user in the DB and returns Auth token.
      *
      * @return void
      */
     public function testLogin()
     {
-        assert(User::all()->count(), 0);
         $user = factory(User::class, 'user')->create(
-          ['password' => bcrypt($password = 'some_password'),]
+            ['password' => bcrypt($password = 'some_password')]
         );
-        assert(User::all()->count(), 1);
+        $this->assertDatabaseHas('users', $user->toArray());
 
         $response = $this
-          ->actingAs($user)
-          ->json('POST', '/api/v1/auth/login',
-          [
-            'email' => $user->email,
-            'password' => $password
-          ]
+            ->actingAs($user)
+            ->json('POST', '/api/v1/auth/login',
+            [
+                'email' => $user->email,
+                'password' => $password
+            ]
         );
         $response
             ->assertStatus(200)
             ->assertSee('success')
             ->assertSee('token');
+    }
+
+    /**
+     * Test ResetPassword endpoint.
+     *
+     * Matches given token and email address to
+     * password reset reservation from DB.
+     *
+     * @return void
+     */
+    public function testResetPassword()
+    {
+        $user = factory(User::class, 'user')->create(
+            ['password' => bcrypt($password = 'some_password')]
+        );
+        DB::table('password_resets')->insert([
+            'email' => $user->email,
+            'token' => 'some_token',
+            'created_at' => Carbon::now()
+        ]);
+        $original_hash = User::where('email', $user->email)
+            ->first()->password;
+        $this->assertDatabaseHas('users',
+            ['password' => $original_hash]
+        );
+
+        $response = $this
+            ->actingAs($user)
+            ->json('POST', '/api/v1/user/resetPassword',
+            [
+                'email' => $user->email,
+                'password' => 'some_other_password',
+                'token' => 'some_token'
+            ]
+        );
+        $response->assertStatus(200);
+        // user still exists
+        $this->assertDatabaseHas('users', $user->toArray());
+        // password hash has changed
+        $this->assertDatabaseMissing('users',
+            ['password' => $original_hash]
+        );
     }
 }
