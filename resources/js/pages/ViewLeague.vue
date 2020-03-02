fb3fa1b52bf794ba53d2fbc83b0571d59e1d931e
Brian Wentzloff <brian.wentzloff@gmail.com>
2020-03-01 21:12:43 -0600

    quick triage attempt

--- resources/js/pages/ViewLeague.vue
+++ resources/js/pages/ViewLeague.vue
@@ -2403,7 +2403,7 @@ import moment from 'moment'
                 total += players[lineup].score;
 
             }
-            return total;
+            return Number(total);
         },
         calculateMatchupScores() {
             this.matchup_home_total = 0;
