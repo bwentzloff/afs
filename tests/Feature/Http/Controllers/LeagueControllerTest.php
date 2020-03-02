96fea6998b246b5cb920142265b7693e52943d26
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-03-01 23:31:45 -0800

    Add test for updateDraftStatus endpoint

--- tests/Feature/Http/Controllers/LeagueControllerTest.php
+++ tests/Feature/Http/Controllers/LeagueControllerTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\Http\Controllers;
 
+use App\User;
+use App\Models\League;
+use App\Models\LeagueUser;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,10 +16,43 @@ class LeagueControllerTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testUpdateDraftStatus()
     {
-        $response = $this->get('/');
+        $users = array(
+            factory(User::class, 'user')->create(),
+            factory(User::class, 'user')->create(),
+            factory(User::class, 'user')->create(),
+            factory(User::class, 'user')->create(),
+        );
 
-        $response->assertStatus(200);
+        $league = factory(League::class, 'league')->create();
+
+        foreach ($users as $index => $user) {
+            factory(LeagueUser::class, 'league_user')
+                ->create(['draft_order' => $index]);
+        }
+        $this->assertDatabaseHas('leagues',
+            ['draft_status' => 0]);
+
+        // this endpoint calls "createDraftPicks"
+        $this->json('POST', '/api/v1/league/updateRoster',
+            [
+                'leagueId' => $league->id,
+                'qbs' => 1,
+                'rbs' => 1,
+                'wrs' => 1,
+                'tes' => 1,
+                'flex' => 1,
+                'superflex' => 0,
+                'ks' => 1,
+                'def' => 1,
+                'bench' => 2,
+                'teamQbs' => 0,
+                'teamKs' => 0
+            ]
+        );
+        $response = $this->get('/api/v1/league/updateDraftStatus');
+        $this->assertDatabaseHas('leagues',
+            ['draft_status' => 1]);
     }
 }
